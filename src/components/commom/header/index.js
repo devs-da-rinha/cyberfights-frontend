a991bb7f14234fc6f4fbe4e673560d4aa2a8d54c
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-09-28 22:13:32 -0300

    Add files via upload

--- src/components/commom/header/index.js
+++ src/components/commom/header/index.js
@@ -1,6 +1,5 @@
+import { Button, Layout, Typography } from "antd";
 import React from "react";
-import { Layout, Row } from "antd";
-import { Typography } from "antd";
 
 const HeaderAntd = Layout.Header;
 const { Title } = Typography;
@@ -8,7 +7,11 @@ const { Title } = Typography;
 const Header = () => {
   return (
     <HeaderAntd style={{ display: "flex", alignItems: "center" }}>
-        <Title>Oieeee</Title>
+        <Title>CyberFights</Title>
+        <div className="ms-auto justify-end">
+          <Button href="">Acesso</Button>
+          <Button className="ml-3">Cadastro</Button>
+        </div>
     </HeaderAntd>
   );
 };
